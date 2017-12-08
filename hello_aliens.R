ccbb25154d61de6b282cb27f0862e3e4a70d7b0b
Yu Wu <yuwu@Yus-MacBook-Pro.wv.cc.cmu.edu>
2017-12-08 11:59:41 -0500

    message to aliens

---
+++ hello_aliens.R
@@ -0,0 +1,4 @@
+
+message <- "Hello, this is a message to our alien friends.\nI do not know if this message would ever be delivered, and I am not sure if I hope it be.\nSome people think it is too lonely for Earth to be the only planet that has given birth to highly intelligent animals.\nOthers take pride in this understanding. From my point of view, this is most likely a problem to be solved long after the end of my life."
+
+cat(message) 
